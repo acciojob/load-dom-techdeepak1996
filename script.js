657a4f124c1961658cc40c41d5551eba3c3a0171
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-06 23:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-            const message = document.createElement("p");
-            message.textContent = "DOM load success";
-            document.body.appendChild(message);
-        });
+    const message = document.createElement("p");
+    message.textContent = "DOM load success";
+    document.body.appendChild(message);
+});
